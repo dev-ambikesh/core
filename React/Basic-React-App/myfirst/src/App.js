8328c7ee43f81770b74eefe5b2756ec0e0c815ba
Ambikesh <developer.ambikesh@gmail.com>
2022-01-31 00:34:29 +0530

    My first Commit

---
+++ React/Basic-React-App/myfirst/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Heading from './heading';
+import Paragraph from './paragraph';
+import My_table from './table';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading/>
+      <Paragraph/>
+      <My_table/>       
+    </div>
+  );
+}
+
+export default App;
